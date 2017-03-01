dbbfa6a0ad569d845733b3abd1b936cd9ca10a19
Elias Bencz <eliasbencz@gmail.com>
2017-03-01 12:21:48 -0300

    Don't shut unless the target will be inside of batlefield.

--- Foo.java
+++ Foo.java
@@ -26,13 +26,19 @@ public class Foo extends Robot
 			return newHeading;
 		}
 		
+		boolean insideOfBattleField;
+		public boolean getInsideOfBattleField()
+		{
+			return insideOfBattleField;
+		}
+		
 		private double getBulletVelocity(double firepower)
 		{
 			return 20 - 3 * firepower;
 		}
 		
 		private double x, y;
-		public PredictRobotEvent(ScannedRobotEvent e, double firepower, double Heading) {
+		public PredictRobotEvent(ScannedRobotEvent e, Robot robot, double firepower) {
 			double dxdt, dydt, angle;
 			double nx, ny, dt;
 			
@@ -43,7 +49,7 @@ public class Foo extends Robot
 			y = e.getDistance() * Math.cos(angle);
 			x = e.getDistance() * Math.sin(angle);
 			
-			double head2 = e.getHeading() - Heading;
+			double head2 = e.getHeading() - robot.getHeading();
 			dxdt = Math.sin(Math.toRadians(head2)) * e.getVelocity();
 			dydt = Math.cos(Math.toRadians(head2)) * e.getVelocity();
 
@@ -58,10 +64,17 @@ public class Foo extends Robot
 
 			ny = y + dydt * dt;
 			nx = x + dxdt * dt;
-			
+						
 			double newAngle = Math.atan2(nx, ny);
 			newBearing = Math.toDegrees(newAngle);
 			newDistance = Math.hypot(ny, nx);
+			
+			//Computes if the target will be outside of the BattleField
+			angle = Math.toRadians(newBearing + robot.getHeading());
+			y = newDistance * Math.cos(angle) + robot.getY();
+			x = newDistance * Math.sin(angle) + robot.getX();
+			
+			insideOfBattleField = x > 0 && y > 0 && x < robot.getBattleFieldWidth() && y < getBattleFieldHeight();
 		}
 	}
 
@@ -107,26 +120,23 @@ public class Foo extends Robot
 		if (e.getName() != target.name)
 			return;
 
-		PredictRobotEvent pe = new PredictRobotEvent(e, 1, getHeading());
+		PredictRobotEvent pe = new PredictRobotEvent(e, this, 1);
 					
 		target.counter++;
 		target.distance = e.getDistance();
-		
-		double diff = getGunHeading() - getHeading();
-		double adjust = (diff - e.getBearing() + 540) % 360 - 180;
-		double padjust = (diff - pe.getBearing() + 540) % 360 - 180;		
-		
+	
 		double diff_radar = getRadarHeading() - getHeading();
 		double adjust_radar = (e.getBearing() - diff_radar + 540) % 360 - 180;
 		target.adjust = adjust_radar;
 
-		//target.x = pe.x;
-		//target.y = pe.y;
-		//out.println("my x:" + getX() + " my y:" + getY());
+		if (pe.getInsideOfBattleField() == false)
+			return;
 
-		out.println("Adjust:" + adjust + " pAdjust:" + padjust);
-		if (Math.abs(adjust) > 0.01) {
-			turnGunLeft(padjust);
+		double diff = getGunHeading() - getHeading();
+		double adjust_gun = (diff - pe.getBearing() + 540) % 360 - 180;
+		
+		if (Math.abs(adjust_gun) > 0.01) {
+			turnGunLeft(adjust_gun);
 		}
 
 		if (getGunHeat() == 0) {
